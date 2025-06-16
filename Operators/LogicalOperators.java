8545644a1eac84e7eef82720ae4d765d30020e93
sravanthi-99 <parisesravanthi0@gmail.com>
2025-06-16 14:57:29 +0530

    Add files via upload

---
+++ Operators/LogicalOperators.java
@@ -0,0 +1,14 @@
+//Logical Operators
+import java.util.*;
+public class LogicalOperators
+{
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  boolean a=sc.nextBoolean();
+  boolean b=sc.nextBoolean();
+  System.out.println(a&&b);
+  System.out.println(a||b);
+  System.out.println(!a);
+ }
+}
---
+++ Operators/UnaryOperators.java
@@ -0,0 +1,14 @@
+//Unary Operators
+import java.util.*;
+public class UnaryOperators
+{
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  int a=sc.nextInt();
+  System.out.println(a++);
+  System.out.println(a--);
+  System.out.println(++a);
+  System.out.println(--a);
+ }
+}
